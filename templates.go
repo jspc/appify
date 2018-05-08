203f9c9cede0be09b3bab0612a195cb211e631ee
jspc <james@zero-internet.org.uk>
2018-05-08 14:15:42 +0100

    Introduce whitespace to aid readability

---
+++ templates.go
@@ -0,0 +1,31 @@
+package main
+
+const (
+	infoPlistTemplate = `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+	<dict>
+		<key>CFBundlePackageType</key>
+		<string>APPL</string>
+		<key>CFBundleInfoDictionaryVersion</key>
+		<string>6.0</string>
+		<key>CFBundleName</key>
+		<string>{{ .Name }}</string>
+		<key>CFBundleExecutable</key>
+		<string>{{ .Executable }}</string>
+		<key>CFBundleIdentifier</key>
+		<string>{{ .Identifier }}</string>
+		<key>CFBundleVersion</key>
+		<string>{{ .Version }}</string>
+		<key>CFBundleGetInfoString</key>
+		<string>{{ .InfoString }}</string>
+		<key>CFBundleShortVersionString</key>
+		<string>{{ .ShortVersionString }}</string>
+		{{ if .IconFile -}}
+		<key>CFBundleIconFile</key>
+		<string>{{ .IconFile }}</string>
+		{{- end }}
+	</dict>
+</plist>
+`
+)
